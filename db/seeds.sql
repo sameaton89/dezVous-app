6f3df87caac55e4882827ef281bdb9849c6f7a52
Sam Eaton <sameaton1989@gmail.com>
2018-12-15 15:58:39 -0500

    Adding completed seeds.sql to db directory

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,5 +13,45 @@ Miami, FL 33155", "https://menacatering.com/", "(305) 666-8545"),
 ("Elite Catering", "130 SW 57th Ave
 Miami, FL 33144", "http://elite-catering.com/", "(305) 342-5655
 "),
-("Sam's Slop Bucket", "934 NW. 38th St., Miami, FL 33127", "http://www.slopbucket.net", "(30)")
-;
+("Sam's Slop Bucket", "934 NW. 38th St., Miami, FL 33127", "http://www.slopbucket.net", "(305) 444-3655"),
+("Mr. Pasta Catering", "7415 NW 54th St
+Miami, FL 33166", "https://www.mrpastacatering.com/", "(305) 661-5200");
+
+INSERT INTO locations (name, address, website, phone_number)
+VALUES ("Fairchild Tropical Botanic Garden", "
+10901 Old Cutler Road Coral Gables, FL 33156", "https://www.fairchildgarden.org/", "305-667-1651"),
+("Matheson Hammock Park", "9610 Old Cutler Road Miami, FL", "http://www.miamidade.gov/parks/matheson-hammock.asp", "305-665-5475"),
+("Villa Woodbine", "2167 S Bayshore Dr
+Miami, FL 33133", "http://villa-woodbine.com/", "(305) 858-6660
+"),
+("Longan's Place", "15255 SW 200th St
+Miami, FL 33187", "http://www.longansplace.com/", "(305) 735-1514
+"),
+("The Curtiss Mansion", "500 Deer Run
+Miami Springs, FL 33166", "https://curtissmansion.com/", "305.869.5180 "),
+("Polish American Club", "1250 NW 22nd Ave
+Miami, FL 33125", "https://polishamericanclubmiami.com/", "305-635-2240"),
+("Joseph Club", "1971 NW 7th St
+Miami, FL 33125", "http://www.jclubmia.us", "305-639-8115"),
+("Tootsie's Cabaret", "150 NW 183rd St
+Miami, FL 33169", "https://www.tootsiescabaret.com/", "(305) 651-5822"),
+("Miami Beach Convention Center", "1901 Convention Center Dr, Miami Beach, FL 33139", "http://www.miamibeachconvention.com/", "(786) 276-2600"),
+("Mana Wynwood Convention Center", "318 NW 23rd St, Miami, FL 33127", "http://www.manawynwood.com/", "(305) 573-0371");
+
+INSERT INTO decoration_services (name, address, website, phone_number)
+VALUES ("Rumbas Event Rentals", "10505 NW 29th Ter
+Doral, FL 33172", "https://rumbaseventrentals.com/", "(305) 380-8088"),
+("Terrence's Tent Co.", "494 Flagler St., Miami, FL 33131", "http://www.terrencestents.com", "305-323-4044"),
+("DeAndre's Decorations", "293 NW 180th St., Miami, FL", "http://www.deandredecorates.biz", "305-234-9543"), ("Sensational Experiences Events", "7171 Coral Way
+Miami, FL 33155", "http://www.sensationalexperiences.com/", "786.409.2297"
+),
+("Super Fun Party Rentals", "15448 SW 62nd St
+Miami, FL 33193", "http://superfunpartyrental.com/", "(786) 355-8800"), ("Bert's Bounce Houses", "209 NE 1st St., Miami, FL, 33130", "http://www.bertbounce.gov", "305-324-8558"),
+("Chadwick's Chair Empire", "483 NW 72 Ave., Miami, FL 33056", "http://www.chadwickdachairman.com", "786-884-2923"),
+("Amazing Parties", "7322 SW 109th Ct
+Miami, FL 33173", "http://www.amazingpartiesmiami.com/", "305-595-0007"),
+("Betty Bu Party Rentals", "4201 SW 72nd Ave
+Miami, FL 33155", "https://www.bettybupartymiami.com/", "(305) 559-0777"),
+("Papi's Party Palisades", "2398 SW 8th St., Miami, FL, 33130", "http://www.papipartyin.org", "786-344-2044"),
+("CinnamonStixx Party Rentals", "16633 SW 117th Ave
+Miami, FL 33177", "http://cinnamonstixxparties.com/webnew/", "(786) 263-9334");
